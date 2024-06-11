0a36f8e22becbe40f4fa4ec16a70022511258fdb
Olawale Ogundeji <abiodungndj@gmail.com>
2024-06-11 20:34:43 +0100

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from src.ImageClassifier import logger
+from ImageClassifier import logger
+from ImageClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is my custom log")
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
